0724ccfde45c1a670d4dd6355953ee3704040a4c
cdelro28 <149708117+cdelro28@users.noreply.github.com>
2024-08-03 17:46:16 -0400

    Improved the User Experience (UX)

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Peom is generated!");
   new Typewriter("#generated-poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -11,17 +10,15 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let userInput = document.querySelector("#user-input");
-
   let apiKey = "ba054cd6f15a15c42o3b84c5df16bbtf";
   let context = `User input: Generate a poem using ${userInput.value}`;
   let prompt =
-    "You are a cleaver and professional poem writer. Your mission is to create a short poem in basic HTML based on the user input. Each line separated with a <br/>. Do not include a title or a heading. After the poem, always sign <br/> 'SheCodes AI' inside a <strong> element or in bold.";
+    "You are a cleaver and professional poem writer. Your mission is to create a short poem in basic HTML based on the user input. Each line separated with a <br/>. Do not include a title or a heading. After the poem, always sign <br/> 'SheCodes AI' always inside a <strong> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("generating poem...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
   axios.get(apiUrl).then(displayPoem);
+  let poemElement = document.querySelector("#generated-poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating your '${userInput.value}' poem...`;
 }
 
 let poemFormElement = document.querySelector("#poem-form-input");
